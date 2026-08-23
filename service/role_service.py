742e690fc0b6d2f8f520a0e3b750f3af4d5f03c1
AnantParabDev <anantparab1404@gmail.com>
2026-08-23 16:45:29 +0530

    basic authentication complete with jwt token

---
+++ service/role_service.py
@@ -0,0 +1,27 @@
+from config.database import db
+from models.role import Role
+
+class RoleService:
+  def __init__(self, dao):
+    self.role_dao = dao
+
+  def add_role(self,role_id,roles):
+    role=Role(role_id=role_id, roles=roles)
+    return self.role_dao.add_role(role)
+
+  
+  def get_all_role(self):
+    return self.role_dao.get_all_role()
+  
+  def get_by_rolename(self, name):
+    return self.role_dao.get_by_rolename(name)
+
+  def get_role_by_id(self, r_id):
+    return self.role_dao.get_role_by_id(r_id)
+
+  def update_role(self, role):
+    return self.role_dao.update_role(role)
+
+  def delete_role(self, u_id):
+    role=self.role_dao.get_role_by_id(u_id)
+    return self.role_dao.delete_role(role)
